0522a3bacefd0f893c75fa7a261af1eecafe6c57
Craig Bowman <cbowman@appdev.yearup.org>
2024-10-21 14:26:33 -0500

    created hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+print ('Hello World')
+
+
+message = 'Hello world!'
+print (message)
+
+dollars = 3
+cents = .50
+
+print (dollars + cents)
+
+
+dollars = '3 dollars'
+cents = 'fifty cents'
+
+print (dollars + ' ' + cents)
