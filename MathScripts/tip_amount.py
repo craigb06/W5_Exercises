e37f918350dc69ac51eb05c75919afd717c83eb7
Craig Bowman <cbowman@appdev.yearup.org>
2024-10-23 14:01:29 -0500

    2A lab 2 tip amount completed

---
+++ MathScripts/tip_amount.py
@@ -0,0 +1,9 @@
+# Define the known values
+total = 320
+gratuity = .20
+
+# Calculate the unknown
+tip_amount = total * gratuity
+
+# Display the results
+print('The tip on a $' + str(total) + ' restaurant bill is $' + format(tip_amount, '.2f'))
