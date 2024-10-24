f8958bacb868b00314f52927aa822eae75f5ba27
Craig Bowman <cbowman@appdev.yearup.org>
2024-10-23 21:33:48 -0500

    2B lab 1 rentals completed

--- MathScripts/rentals.py
+++ MathScripts/rentals.py
@@ -19,3 +19,13 @@ print(vans_needed)
 print(vans_cost)
 print(cost_per_person)
 print('With ' + str(tourists) + ' tourists we need ' + str(vans_needed) + ' vans which will cost a total of $' + vans_cost + ' which brings it to $' + cost_per_person + ' per person')
+
+# How much money did your script say you had to charge per person?  $19.74
+
+# If you multiply that out, how much did you collect?  $750.12
+
+# How much were the vans?  $750
+
+# Why do you have leftover money?  
+# I have money leftover because I have to round up when doing the equation to make sure the expenses for the van are covered,
+# the format() method automatically rounds up to a fixed decimal place.
