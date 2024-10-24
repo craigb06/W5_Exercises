345b73c22552c2c73f4cf27f2a15585eda7ef9e0
Craig Bowman <cbowman@appdev.yearup.org>
2024-10-24 16:09:05 -0500

    3B lab 1 movie list midway complete

--- SampleCollections/movie_list.py
+++ SampleCollections/movie_list.py
@@ -1,3 +1,10 @@
 # Define known values
 fav_movies = ['Friday Night Lights', 'Any Given Sunday', 'Remember The Titans', 'Your Name', 'The Notebook']
-moves_to_watch = ['Trap', 'Blink Twice']
+movies_to_watch = ['Trap', 'Blink Twice']
+
+# Calculate the unknown
+fav_movie_len = len(fav_movies)
+movie2watch_len = len(movies_to_watch)
+
+# Display the results
+print('The list ' + str(fav_movies) + 'fave movies includes my top ' + str(fav_movie_len) + ' favorite movies')
