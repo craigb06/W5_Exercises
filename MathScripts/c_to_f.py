09c9e525dc5bc55242c20b97eef9aa0aeeb3e881
Craig Bowman <cbowman@appdev.yearup.org>
2024-10-23 19:00:58 -0500

    2B lab 1 c to f completed

---
+++ MathScripts/c_to_f.py
@@ -0,0 +1,8 @@
+# Define known values
+celsius = 25
+
+# Calculate the unknown
+fahrenheit = (celsius * (9/5)) + 32
+
+# Display the results
+print('25 degrees celsius is ' + format(fahrenheit, '.2f') + ' degrees fahrenheit')
