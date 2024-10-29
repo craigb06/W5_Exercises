eece386b72f3b27a159515483ec5f6b7cefcd306
Craig Bowman <cbowman@appdev.yearup.org>
2024-10-29 09:54:19 -0500

    4B lab 1 completed

---
+++ IfScripts/dept_converter.py
@@ -0,0 +1,16 @@
+# Define the known values
+dept_name = int(input('Department code? '))
+
+# Calculate the unknown
+if dept_name == 1:
+    print('Marketing')
+elif dept_name == 5:
+    print('Human Resources')
+elif dept_name == 10:
+    print('Accounting')
+elif dept_name == 12:
+    print('Legal')
+elif dept_name == 18:
+    print('IT')
+elif dept_name == 20:
+    print('Customer Relations')
