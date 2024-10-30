702dc7200be31b1e3532bc540f7ec17d862622a8
Craig Bowman <cbowman@appdev.yearup.org>
2024-10-29 22:57:37 -0500

    4B lab 5 incomplete need assistance

---
+++ IfScripts/show_major.py
@@ -0,0 +1,20 @@
+# Define known values
+student = [
+    {'student name': 'Craig', 'student major': 'ENG'},
+    {'student_name': 'John', 'student_major': 'BIOL'},
+    {'student_name': 'Jane', 'student_major': 'CSCI'},
+    {'student_name': 'Tim', 'student_major': 'HIST'},
+    {'student_name': 'Tom', 'student_major': 'MKT'}
+]
+
+student_major = [
+    {'BIOL': 'Biology', 'dept_office': 'Science Bldg, Room 310'},
+    {'CSCI': 'Computer Science', 'dept_office': 'Sheppard Hall, Room 314'},
+    {'ENG': 'English', 'dept_office': 'Kerr Hall, Room 201'},
+    {'HIST': 'History', 'dept_office': 'Kerr Hall, Room 114'},
+    {'MKT': 'Marketing', 'dept_office': 'Westly Hall, Room 310'}
+]
+
+# Calculate unknown values
+student_name = str(input())
+if student_major 
